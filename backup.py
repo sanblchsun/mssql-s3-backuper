ef39db9ee7882954849733db744a9d87086661cc
Makosov Aleksander <logovopost@yandex.ru>
2021-11-21 23:10:16 +0300

    working backup.py

--- backup.py
+++ backup.py
@@ -20,8 +20,8 @@ S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 BACKUP_KEY_PUB_FILE = os.getenv("BACKUP_KEY_PUB_FILE")
 TIME_ZONE = os.getenv("TIME_ZONE", "Europe/Moscow")
 
-DB_FILENAME = "/tmp/backup_db.sql.gz.enc"
-
+DB_FILENAME = "/media/d/backup/backup_db.sql.gz.enc"
+location = '/media/d/backup'
 
 def say_hello():
     print(colored("Hi! This tool will dump PostgreSQL database, compress \n"
@@ -45,30 +45,26 @@ def check_key_file_exists():
 
 def dump_database():
     print("\U0001F4E6 Preparing database backup started")
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + DB_HOSTNAME + ';'
-                            'DATABASE=master;UID=' + DB_USER + ';PWD=' + DB_PASSWD)
+    print(f'DB_HOSTNAME:{DB_HOSTNAME}, DB_USER:{DB_USER}, DB_PASSWD:{DB_PASSWD}')
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+DB_HOSTNAME+';'
+                                'DATABASE=master;UID='+DB_USER+';PWD='+ DB_PASSWD)
+    print('connect yes')
     cnxn.autocommit = True
     cursor = cnxn.cursor()
-    TODAY = get_now_datetime_str()
-    backup = "BACKUP DATABASE [" + DB_NAME + "] TO DISK = N'" + DB_FILENAME + "/" + DB_NAME + '-' + str(
-        TODAY) + ".bak'"
 
-    cursor.execute(f"{backup} | gzip -c --best | \
-        openssl smime -encrypt -aes256 -binary -outform DEM \
-        -out {DB_FILENAME} {BACKUP_KEY_PUB_FILE}")
-
-
-    # dump_db_operation_status = os.WEXITSTATUS(os.system(
-    #     f"pg_dump -h {DB_HOSTNAME} -U {DB_USER} {DB_NAME} | gzip -c --best | \
-    #     openssl smime -encrypt -aes256 -binary -outform DEM \
-    #     -out {DB_FILENAME} {BACKUP_KEY_PUB_FILE}"
-    # ))
-
-
-    # if dump_db_operation_status != 0:
-    #     exit(f"\U00002757 Dump database command exits with status "
-    #          f"{dump_db_operation_status}.")
-    # print("\U0001F510 DB dumped, archieved and encoded")
+    path = f"{location}/{DB_NAME}-{get_now_datetime_str()}.bak"
+    print(f'DB_NAME: {DB_NAME}, path: {path}')
+    backup = f"BACKUP DATABASE [{DB_NAME}] TO DISK = N'{path}' WITH FORMAT, STATS=1"
+    cursor.execute(backup)
+    str_cmd = f"openssl smime -encrypt -aes256 -binary -outform DEM -in {path} -out {DB_FILENAME} {BACKUP_KEY_PUB_FILE}"
+    dump_db_operation_status = os.WEXITSTATUS(os.system(str_cmd))
+    cursor.close()
+    if dump_db_operation_status != 0:
+        exit(f"\U00002757 Dump database command exits with status "
+             f"{dump_db_operation_status}.")
+    print("\U0001F510 DB dumped, archieved and encoded")
+        # while cursor.nextset():
+        #     pass
 
 
 def get_s3_instance():
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import pyodbc
 import datetime
 import gzip
+import os
 
 server = 'localhost'
 username = 'SA'
 password = 'Qwer1234'
 location = '/media/d/backup'
+DB_FILENAME = "/media/d/backup/backup_db.sql.gz.enc"
+BACKUP_KEY_PUB_FILE = "backup_key.pem.pub"
 
 TODAY = datetime.date.today()
 SKIP = ['master', 'tempdb', 'model', 'msdb']
@@ -23,9 +26,20 @@ for database in databases:
     database_name = database[0]
     if database_name not in SKIP:
         cursor = cnxn.cursor()
-        backup = "BACKUP DATABASE [" + database_name + "] TO DISK = N'" + location + "/" + database_name + '-' + str(TODAY) + ".bak'"
-        # cursor.execute(backup)
-        cursor.execute(f"{backup} | gzip -c --best -out {location}")
+        path = location + "/" + database_name + '-' + str(TODAY) + ".bak"
+        # backup = "BACKUP DATABASE [" + database_name + "] TO DISK = N'" + path
+        backup = f"BACKUP DATABASE [{database_name}] TO DISK = N'{path}' WITH FORMAT, STATS=1"
+        cursor.execute(backup)
+        str_cmd = f"openssl smime -encrypt -aes256 -binary -outform DEM -in {path} -out {DB_FILENAME} {BACKUP_KEY_PUB_FILE}"
+
+        dump_db_operation_status = os.WEXITSTATUS(os.system(str_cmd))
+        print(str_cmd)
+        if dump_db_operation_status != 0:
+            exit(f"\U00002757 Dump database command exits with status "
+                 f"{dump_db_operation_status}.")
+        print("\U0001F510 DB dumped, archieved and encoded")
+
+        # dump_db_operation_status = os.WEXITSTATUS(os.system(cursor.execute(backup)))
         while cursor.nextset():
             pass
         cursor.close()
